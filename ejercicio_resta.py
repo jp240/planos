6ef0ba43e488717d7d48b2af9ba1441941a019c7
jp <jperdomof24@gmail.com>
2023-02-15 11:27:30 -0500

    se sube cometario y resta

---
+++ ejercicio_resta.py
@@ -0,0 +1,17 @@
+number_one = 68
+number_two = 3
+
+
+#this is a comment of one line 
+#operacion de sumar los dos numeros 
+
+""""
+this is a multyi_line comment
+sumar = number_one + numbre_two
+
+print(sumar)
+
+"""
+
+resta = number_one - number_two
+print(resta)
