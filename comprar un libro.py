5fc536dd99556ac275d58fd8612e40f84bcdee56
jp240 <125463976+jp240@users.noreply.github.com>
2023-03-01 11:38:24 -0500

    Add files via upload

---
+++ comprar un libro.py
@@ -0,0 +1,21 @@
+precio_libro = 2000
+
+if precio_libro == 1000:
+    print('valor del libro vercion grande tiene un total:1000')
+elif precio_libro == 2000:
+    print('valor del libro pasta vercion grande dura es:2000')
+elif precio_libro == 100: 
+    print('valor de libro vercion mas pequeña es:100')
+elif precio_libro == 200:
+    print('valor de libro vercion mas pequeña pasta dura es: 200')
+
+else: 
+    print('error')
+
+
+
+
+
+
+
+
